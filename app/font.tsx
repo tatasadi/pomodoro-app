9602932fc6e2d990c0d1a783d347b066769eeee9
Ehsan Tatasadi <ehsan@tatasadi.com>
2024-11-28 18:09:07 +0100

    add setting dialog

---
+++ app/font.tsx
@@ -0,0 +1,17 @@
+import { Kumbh_Sans, Roboto_Slab, Space_Mono } from 'next/font/google'
+
+export const kumbhSans = Kumbh_Sans({
+	subsets: ['latin'],
+	display: 'swap',
+})
+
+export const robotoSlab = Roboto_Slab({
+	subsets: ['latin'],
+	display: 'swap',
+})
+
+export const spaceMono = Space_Mono({
+	subsets: ['latin'],
+	display: 'swap',
+	weight: ['700'],
+})
