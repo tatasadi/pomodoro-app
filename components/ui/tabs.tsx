a93d55b100be6e41eb3682c3315f17991ce7529d
Ehsan Tatasadi <ehsan@tatasadi.com>
2024-12-03 17:43:55 +0100

    change font size for tabs

--- components/ui/tabs.tsx
+++ components/ui/tabs.tsx
@@ -32,7 +32,7 @@ const TabsTrigger = React.forwardRef<
 		<TabsPrimitive.Trigger
 			ref={ref}
 			className={cn(
-				`inline-flex items-center justify-center whitespace-nowrap rounded-full px-3 py-1 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-[var(--selected-color)] data-[state=active]:text-background data-[state=active]:shadow`,
+				`inline-flex items-center justify-center whitespace-nowrap rounded-full px-3 py-1 text-xs sm:text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-[var(--selected-color)] data-[state=active]:text-background data-[state=active]:shadow`,
 				className,
 			)}
 			{...props}
