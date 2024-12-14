c2cad926631110f1046ad3536f99cc355a59e059
Ehsan Tatasadi <ehsan@tatasadi.com>
2024-12-14 08:31:12 +0100

    fix local storage

--- app/use-localstorage.tsx
+++ app/use-localstorage.tsx
@@ -1,31 +1,23 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 function useLocalStorage<T>(key: string, initialValue: T) {
-	// State to store the current value
-	const [storedValue, setStoredValue] = useState<T>(() => {
+	const [storedValue, setStoredValue] = useState<T>(initialValue)
+
+	useEffect(() => {
 		try {
-			// Check if the value already exists in local storage
 			const item = window.localStorage.getItem(key)
-			// If it exists, parse it; otherwise, set the initial value
 			if (item) {
-				return JSON.parse(item)
-			} else {
-				// Set initial value in local storage
-				window.localStorage.setItem(key, JSON.stringify(initialValue))
-				return initialValue
+				setStoredValue(JSON.parse(item))
 			}
 		} catch (error) {
 			console.error('Error accessing local storage', error)
-			return initialValue
 		}
-	})
+	}, [key])
 
-	// Function to update the stored value
 	const setValue = (value: T | ((val: T) => T)) => {
 		try {
 			const valueToStore = value instanceof Function ? value(storedValue) : value
 			setStoredValue(valueToStore)
-			// Save to local storage
 			window.localStorage.setItem(key, JSON.stringify(valueToStore))
 		} catch (error) {
 			console.error('Error writing to local storage', error)
